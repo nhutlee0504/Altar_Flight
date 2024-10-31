a8e076fa5f595284c5ea626bd64047f2d91d4694
nhutlee0504 <leminhnhut718@gmail.com>
2024-10-31 14:59:18 +0700

    31/10/2024 hoàn thành

---
+++ API_Flight_Altar_ThucTap/API_Flight_Altar_ThucTap/Migrations/20241031075825_Dbv12.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_Flight_Altar_ThucTap.Migrations
+{
+    public partial class Dbv12 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
